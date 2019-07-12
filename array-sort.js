e0c97b1d72023f0662259f52c413a94d7bd976ec
Damodharan <damu.be@gmail.com>
2019-07-12 14:49:33 +0300

    Array Sorting

---
+++ array-sort.js
@@ -0,0 +1,14 @@
+var a = [2,4,6,7,2,8,16,8];
+
+// SOER THE ARRAY 
+a.sort();
+
+console.log("ANS==>",a);
+//Otput "ANS==>" [16, 2, 2, 4, 6, 7, 8, 8]
+
+
+// SORT in reverse Order 
+console.log("ANS==>", a.reverse());
+// OUTPUT: "ANS==>" [8, 8, 7, 6, 4, 2, 2, 16]
+
+//===============================================================
